478e5f591166107d5f94b121a0826bf40a7d27ef
Breno Martinusso <martinusso@gmail.com>
2020-05-09 00:04:14 -0300

    Develop (#3)
    * add new layout
    
    * refactor desktop
    
    * responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+
+  $(".sample ul li:first-child").toggleClass('active');
+  $(".code-sample-body div:first-child").toggleClass('active');
+
+  $("table tbody tr td").each((el) => {
+    var idx = $("table tbody tr td").eq(el).index();
+    var label =  $("table thead tr th").eq(idx).html();
+    $("table tbody tr td").eq(el).attr('data-label', label);
+  });
+
   $('.menu').click(function() {
     $('.barra-lateral').toggleClass('active');
     $('.overlay').toggleClass('active');
@@ -39,4 +49,15 @@ $(".trigger").click(function() {
 $(".has-accordion a").click(function() {
   $(this).toggleClass('ativo');
   $(this).next('ul').slideToggle('swing');
+});
+
+$(".sample ul li a").click(function(event) {
+  var li = $(event.target).parent();
+  var ul = li.parent();
+  ul.children().removeClass('active');
+  li.toggleClass('active');
+  var i = li.index();
+  var divCode =  ul.parent().find(".code-sample-body");
+  divCode.children().removeClass('active');
+  divCode.children().eq(i).toggleClass('active');
 });
